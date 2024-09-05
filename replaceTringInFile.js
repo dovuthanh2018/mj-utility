199a811041f16cda34a29cbbd137450a5948e67e
iosDeveloper <dovuthanh2018@gmail.com>
2024-09-05 17:56:32 +0700

    updated

--- replaceTringInFile.js
+++ replaceTringInFile.js
@@ -1,35 +1,62 @@
 const fs = require('fs');
 const main = async () => {
 	const args = require('minimist')(process.argv.slice(2));
-	const { path, findString, replaceString} = args;
-	console.log(findString);
-	if (!fs.existsSync(path)) {
-		console.log('file not found: ' + path);
+	const { endpoint, env} = args;
+  // const pathIntl = '/Users/user/.jenkins/workspace/mj-mobile-ios/mj-mobile-ios-build/app/config/intl.ts'
+  // const pathAppConfig = '/Users/user/.jenkins/workspace/mj-mobile-ios/mj-mobile-ios-build/app/config/index.ts'
+  const pathIntl = '/Users/iosdeveloper/Documents/second/mj-mobile/app/config/intl.ts'
+  const pathAppConfig = '/Users/iosdeveloper/Documents/second/mj-mobile/app/config/index.ts'
+	if (!fs.existsSync(pathIntl) || !fs.existsSync(pathAppConfig)) {
+		console.log('file config not found');
 		process.exit();
 	}
-	console.log(findString)
-	console.log(replaceString)
+	console.log(endpoint)
+	console.log(env)
 
-	const envText = 'export const env: Env';
-	const allFileContents = fs.readFileSync(path, 'utf-8');
-	const line = allFileContents.split(/\r?\n/);
-	line.forEach((item) => {
-		if(item.toLocaleString().includes(findString) && item.toLocaleString().includes('http')){
-			const index = line.indexOf(item);
-			line[index] = "const productionEndpoint = '" + replaceString + "';";
+	const envText = 'export const env: env';
+  const defaultConfigText = 'const defaultConfig'
+  const envConfig= env === 'PRODUCTION'? 'PROD_ENV' : 'TEST_ENV';
+
+  //handle intl file
+  const intlFileContents = fs.readFileSync(pathIntl, 'utf-8');
+	const intlLine = intlFileContents.split(/\r?\n/);
+  intlLine.forEach((item) => {
+		if(item.toLowerCase().includes('productionendpoint') && item.toLowerCase().includes('http')){
+			const index = intlLine.indexOf(item);
+      intlLine[index] = "const productionEndpoint = '" + endpoint + "';";
 		}
-		if(item.toLocaleString().includes(envText)){
-			const index = line.indexOf(item);
-			line[index] = "export const env: Env = __DEV__ ? 'dev' : 'production';";
-		}
+    if(item.toLowerCase().includes('localendpoint') && item.toLowerCase().includes('http')){
+      const index = intlLine.indexOf(item);
+      intlLine[index] = "const localEndpoint = '" + endpoint + "';";
+    }
+    if(item.toLowerCase().includes('stagingendpoint') && item.toLowerCase().includes('http')){
+      const index = intlLine.indexOf(item);
+      intlLine[index] = "const stagingEndpoint = '" + endpoint + "';";
+    }
+    if(item.toLowerCase().includes('testingendpoint') && item.toLowerCase().includes('http')){
+      const index = intlLine.indexOf(item);
+      intlLine[index] = "const testingEndpoint = '" + endpoint + "';";
+    }
+  });
+  fs.writeFileSync(pathIntl, intlLine.join("\r\n"));
 
-	});
 
-	fs.writeFileSync(path, line.join("\r\n"));
-	// const content = await fs.read(path, 'utf8').split('/\\r?\\n/');
-	// console.log(content0);
-	// const result = content.ind
+  //handle intl file
+  const configFileContents = fs.readFileSync(pathAppConfig, 'utf-8');
+  const configLine = configFileContents.split(/\r?\n/);
+  configLine.forEach((item) => {
+    if(item.toLowerCase().includes(envText.toLowerCase())){
+      const index = configLine.indexOf(item);
+      configLine[index] = `export const env: Env = '${env.toLowerCase()}'`;
+    }
 
+    if(item.toLowerCase().includes(defaultConfigText.toLowerCase())){
+      const index = configLine.indexOf(item);
+      configLine[index] = `const defaultConfig = ${envConfig};`;
+    }
+
+  });
+  fs.writeFileSync(pathAppConfig, configLine.join("\r\n"));
 };
 
 main();
